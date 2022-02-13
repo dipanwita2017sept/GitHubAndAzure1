c215d612401ca17e389f3f342f0276a88ce4e4c8
dipanwita2017sept <dipanwita2017sept@gmail.com>
2022-02-13 16:36:54 +0530

    Adding Project Files

---
+++ src/main/java/GithubAndAzure1/GithubAndAzure1/App.java
@@ -0,0 +1,13 @@
+package GithubAndAzure1.GithubAndAzure1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
